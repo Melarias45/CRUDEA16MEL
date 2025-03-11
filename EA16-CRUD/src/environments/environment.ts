2cfaf3f1c1b15c2371b6c522579f621466558951
Mel⭐ <22073@portalcedi.edu.mx>
2025-03-11 08:19:24 -0600

    initial commit

---
+++ EA16-CRUD/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDCNDtHZKzTT7RyKa94SqZ7PUr4rw0uWiw",
+    authDomain: "ea16-crud.firebaseapp.com",
+    projectId: "ea16-crud",
+    storageBucket: "ea16-crud.firebasestorage.app",
+    messagingSenderId: "288874667574",
+    appId: "1:288874667574:web:5b8349667b1363d1984297"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
